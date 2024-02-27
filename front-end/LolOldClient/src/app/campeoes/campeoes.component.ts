c0b72ff64e6da00929fe5bbfe43e20085293e9c7
Lucas Hungaro <lucasbelisariohungaro@gmail.com>
2024-02-27 12:41:39 -0300

    Ajustando filtros de campeões

--- front-end/LolOldClient/src/app/campeoes/campeoes.component.ts
+++ front-end/LolOldClient/src/app/campeoes/campeoes.component.ts
@@ -5,35 +5,31 @@ import { AccountService } from '../services/account.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-campeoes',
   templateUrl: './campeoes.component.html',
-  styleUrls: ['./campeoes.component.css']
+  styleUrls: ['./campeoes.component.css'],
 })
 export class CampeoesComponent {
-
   ordenacao: string = 'Alfabética';
-  filtroChamp: any = "";
-  filtrobau: string = "false";
-  Tanque : string = "Tank";
-  Lutador : string = "Fighter";
-  Mage :string = "Mage";
-  Assassin :string = "Assassin";
-  Marksman :string = "Marksman";
-  Support :string = "Support";
+  filtroChamp: any = '';
+  filtrobau: string = 'false';
+  Tanque: string = 'Tank';
+  Lutador: string = 'Fighter';
+  Mage: string = 'Mage';
+  Assassin: string = 'Assassin';
+  Marksman: string = 'Marksman';
+  Support: string = 'Support';
 
-
-
-
-  puuid: string = '5-9UAP0WP_BqWLHurxIjj1PprptYifhFeHfhWL5AHzQDJ4KiGgaspRtz8Zv7rf0ItIR_Zi2aBkH8zQ'
+  puuid: string =
+    '5-9UAP0WP_BqWLHurxIjj1PprptYifhFeHfhWL5AHzQDJ4KiGgaspRtz8Zv7rf0ItIR_Zi2aBkH8zQ';
   champions: Champion[] = [];
-  mastery!: Mastery[];
-  opcaoEscolhida: string = "";
-  classeEscolhida: string = "";
+  todosCampeoes!: Mastery[];
+  opcaoEscolhida: string = '';
+  classeEscolhida: string = '';
 
   // Função para dividir o array em grupos de tamanho 'size'
-  chunk(array:any, size:any) {
+  chunk(array: any, size: any) {
     const result = [];
     for (let i = 0; i < array.length; i += size) {
       result.push(array.slice(i, i + size));
@@ -41,117 +37,134 @@ export class CampeoesComponent {
     return result;
   }
 
-
   constructor(
-
     private accountService: AccountService,
-    private spinner: NgxSpinnerService) {  }
+    private spinner: NgxSpinnerService
+  ) {}
 
-  ngOnInit() {
-
-  }
-  carregarCampeoes(){
-    // this.GetChampions();
+  ngOnInit() {}
+  carregarCampeoes() {
+    this.GetChampions();
     this.GetMasterys();
   }
 
-  filtrarLista() {
-    if(this.filtroChamp != ""){
-      return this.mastery.filter(item => item.champName.toLowerCase().includes(this.filtroChamp.toLowerCase()));
-    }else if (this.opcaoEscolhida != ""){
-      return this.ordenarOpcoes(this.opcaoEscolhida)
-    }else if(this.classeEscolhida != ""){
-      return this.classeChamp(this.classeEscolhida)
+  filtrarLista(): Mastery[] {
+    let campeoes: Mastery[] = [];
+
+    if (this.filtroChamp != '') {
+      campeoes = this.todosCampeoes.filter((item) =>
+        item.champName.toLowerCase().includes(this.filtroChamp.toLowerCase())
+      );
     }
-    else
-      return this.mastery;
+    if (this.opcaoEscolhida != '') {
+      let campeoesParaOrdenar =
+        this.filtroChamp != '' ? campeoes : this.todosCampeoes;
+      campeoes = this.ordenarOpcoes(campeoesParaOrdenar);
+    }
+    if (this.classeEscolhida != '') {
+      let campeoesParaFiltrar =
+        this.filtroChamp != '' || this.opcaoEscolhida != ''
+          ? campeoes
+          : this.todosCampeoes;
+      campeoes = this.classeChamp(campeoesParaFiltrar);
+    }
+
+    if (
+      this.filtroChamp != '' ||
+      this.opcaoEscolhida != '' ||
+      this.classeEscolhida != ''
+    ) {
+      return campeoes;
+    } else {
+      return this.todosCampeoes;
+    }
   }
 
-  GetChampions(){
+  GetChampions() {
     this.spinner.show();
 
     this.accountService.getChampions().subscribe({
-      next: (_account:Champion[]) => {
-        this.champions = _account
+      next: (_account: Champion[]) => {
+        this.champions = _account;
       },
-      error: (error:any) => {
+      error: (error: any) => {
         // this.spinner.hide();
-        console.log('Erro ao carregar o Usuário','Erro!');
+        console.log('Erro ao carregar o Usuário', 'Erro!');
       },
-        complete: () => {
+      complete: () => {
         this.spinner.hide();
-      }
+      },
     });
   }
 
-  GetMasterys(){
-      this.accountService.getMaestriasByPiuuId(this.puuid).subscribe({
-        next: (_mastery:Mastery[]) => {
-          this.mastery = _mastery
-          console.log("Maestrias");
-          console.log(this.mastery);
-        },
-        error: (error:any) => {
-          // this.spinner.hide();
-          console.log('Erro ao carregar as Maestrias','Erro!');
-        },
-         complete: () => {
-          this.spinner.hide();
-        }
-      });
+  GetMasterys() {
+    this.accountService.getMaestriasByPiuuId(this.puuid).subscribe({
+      next: (_mastery: Mastery[]) => {
+        this.todosCampeoes = _mastery;
+        console.log('Maestrias');
+        console.log(this.todosCampeoes);
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        console.log('Erro ao carregar as Maestrias', 'Erro!');
+      },
+      complete: () => {
+        this.spinner.hide();
+      },
+    });
   }
 
-  ordenarOpcoes(opcao: string){
-    switch (opcao){
-
+  ordenarOpcoes(campeoesParaFiltrar: Mastery[]) {
+    switch (this.opcaoEscolhida) {
       case 'Alfabetica':
-        let _champions = this.mastery.sort((a , b) => a.champName.localeCompare(b.champName));
+        let _champions = campeoesParaFiltrar.sort((a, b) =>
+          a.champName.localeCompare(b.champName)
+        );
         return _champions;
 
       case 'Maestria':
-        let _masterys = this.mastery.sort((a , b) =>{
-        if(a.championLevel !== undefined && b.championLevel !== undefined){
-          return b.championLevel.localeCompare(a.championLevel)
-        }
-        return 0;
-      });
+        let _masterys = campeoesParaFiltrar.sort((a, b) => {
+          if (a.championLevel !== undefined && b.championLevel !== undefined) {
+            return b.championLevel.localeCompare(a.championLevel);
+          }
+          return 0;
+        });
 
         return _masterys;
 
-        case 'Bau-disponivel':
-          if(this.filtrobau == "false"){
-            return this.mastery.filter(item => item.chestGranted && item.chestGranted.includes(this.filtrobau));
-
-          }
-        break;
-
-
-      }
-    return this.mastery
-  }
-
-  classeChamp(opcaoChamp: string){
-
-
-    switch(opcaoChamp){
-      case 'Todos':
-
-      break;
-
-      case 'Tank':
-
-        if(this.Tanque === "Tank"){
-          return this.mastery.filter(item =>  item.tags.includes("Tank"));
-
+      case 'Bau-disponivel':
+        if (this.filtrobau == 'false') {
+          return campeoesParaFiltrar.filter(
+            (item) =>
+              item.chestGranted && item.chestGranted.includes(this.filtrobau)
+          );
         }
-      break;
-
-
+        break;
     }
-    return this.mastery
+    return campeoesParaFiltrar;
   }
 
+  classeChamp(campeoesParaFIltrar: Mastery[]): Mastery[] {
+    let tag = this.classeEscolhida;
+    let filtrados: Mastery[] = [];
 
+    campeoesParaFIltrar.forEach((m) => {
+      let champMastery = this.champions.find((c) => c.key == m.championId);
+      let tagEncontrada = 0;
 
+      champMastery?.tags.forEach((champTag) => {
+        if (champTag == tag) tagEncontrada++;
+      });
+
+      if (tagEncontrada > 0) {
+        filtrados.push(m);
+      }
+    });
+
+    if (tag == 'Todos') {
+      return campeoesParaFIltrar;
+    }
+
+    return filtrados;
+  }
 }
-
